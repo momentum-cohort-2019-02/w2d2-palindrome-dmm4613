961d5fe10e286fcd4f59f7c77baa6052ccbd3391
Dan Miller <dmm4613@gmail.com>
2019-02-19 16:24:40 -0500

    added docstrings to the functions

--- palindrome.py
+++ palindrome.py
@@ -2,6 +2,7 @@
 
 
 def strip_input(name):
+    """This will lowercase all letters and then strip nonletters from the string"""
     import re
     print ("strip_input was called")
     #.lower will take the entire string and make all cases lower
@@ -16,6 +17,7 @@ def strip_input(name):
     return name
 
 def is_palindrome(name):
+    """compares the parameter string and its reverse to see if it's palindrome"""
     print ("is_palindrome was called")
     #name[begin:end:step] is outputting the string 'name' in reverse. 
     if strip_input(name) == strip_input(name[::-1]):
@@ -25,8 +27,10 @@ def is_palindrome(name):
 
 while True:
     name = input("Enter a word or sentence and see if it's palindrome [type 'quit' to end]: ")
+    #if the user inputs 'quit' the loop will terminate 
     if name == 'quit':
         break
+    #if the user does not enter anything, they will be prompted to retry, and the loop will restart    
     if name == "":
         print ("You didn't enter anything, let's start over!")
         continue    
